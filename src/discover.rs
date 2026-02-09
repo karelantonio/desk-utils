bb9af93f646efeff79e6519be64907bf3f164483
Karel <karelantoniogz@gmail.com>
2026-02-08 19:03:04 -0500

    feat: Able to discover desktop apps (via the desktop files, see xdg)

---
+++ src/discover.rs
@@ -0,0 +1,167 @@
+use core::ffi::CStr;
+use std::{
+    ffi::OsStr,
+    fs::{DirEntry, File},
+    io::Read,
+    os::unix::ffi::OsStrExt,
+    path::{Path, PathBuf},
+};
+
+use anyhow::bail;
+
+/// Get the XDG data directories from the environment or return the default ones
+fn xdg_data_dirs() -> Vec<String> {
+    match std::env::var("XDG_DATA_DIRS") {
+        Ok(val) => val.split(':').map(Into::into).collect(),
+        Err(err) => {
+            log::debug!(
+                "Environment variable: XDG_DATA_DIRS not found or invalid ({err}), using default (/usr/local/share, /usr/share and ~/.local/share)"
+            );
+            vec![
+                "/usr/local/share".into(),
+                "/usr/share".into(),
+                "~/.local/share".into(),
+            ]
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DesktopEntry {
+    pub name: String,
+    pub icon: Option<String>,
+    pub cmd: String,
+    pub terminal: bool,
+}
+
+pub fn desktop_apps() -> Vec<DesktopEntry> {
+    let mut apps = Vec::new();
+    for dir in xdg_data_dirs() {
+        let fullpath = format!("{dir}/applications");
+        log::debug!("Checking directory '{fullpath}' for desktop entries");
+        let files = match std::fs::read_dir(fullpath) {
+            Ok(dirs) => dirs,
+            Err(err) => {
+                log::warn!("Could not read directory: {err}, skipping");
+                continue;
+            }
+        };
+
+        for entry in files {
+            let entry = match entry {
+                Ok(entry) => entry,
+                Err(err) => {
+                    log::warn!("Could not read entry: {err}, ignoring...");
+                    continue;
+                }
+            };
+
+            let path = entry.path();
+
+            if !path.is_file() {
+                continue;
+            }
+
+            if Some(OsStr::from_bytes(b"desktop")) != path.extension() {
+                continue;
+            }
+
+            // Is a desktop file, parse
+            let entry = match parse_desktop_file(path) {
+                Ok(e) => e,
+                Err(err) => {
+                    log::warn!("Error (ignored): {err}");
+                    continue;
+                }
+            };
+
+            let Some(app) = entry else { continue };
+
+            apps.push(app);
+        }
+    }
+    apps
+}
+
+fn parse_desktop_file(path: PathBuf) -> anyhow::Result<Option<DesktopEntry>> {
+    log::debug!("Parsing file: {path:?}");
+    // First read the file
+    let mut content = Vec::new();
+    let _ = File::open(path)?.read_to_end(&mut content)?;
+    let mut in_desktop_entry = false;
+
+    let mut name = None;
+    let mut icon = None;
+    let mut cmd = None;
+    let mut terminal = false;
+    let mut isapp = false;
+
+    for mut line in content.split(|c| *c == b'\n') {
+        while line.len() > 0 && (line[0] == b' ' || line[0] == b'\t') {
+            line = &line[1..];
+        }
+
+        if line.len() == 0 {
+            // Empty
+            continue;
+        }
+
+        if line.starts_with(b"[") {
+            // Section name
+            in_desktop_entry = line.starts_with(b"[Desktop Entry]");
+            continue;
+        }
+
+        if line.starts_with(b"#") {
+            // Comment
+            continue;
+        }
+
+        if !line.contains(&b'=') {
+            // Not important or invalid i guess
+            continue;
+        }
+        let mut sli = line.splitn(2, |c| *c == b'=');
+        let (Some(bname), Some(bvalue), Option::None) = (sli.next(), sli.next(), sli.next()) else {
+            continue; // Else invalid
+        };
+
+        if !in_desktop_entry {
+            continue;
+        }
+
+        if bname == b"Name" {
+            name = Some(String::from_utf8(bvalue.into())?);
+        } else if bname == b"Icon" {
+            icon = Some(String::from_utf8(bvalue.into())?);
+        } else if bname == b"Type" {
+            isapp = bvalue == b"Application";
+        } else if bname == b"Terminal" {
+            terminal = bvalue == b"true";
+        } else if bname == b"Exec" {
+            if bvalue.contains(&b'%') {
+                // Not supported rn :(, implement later
+                continue;
+            }
+            cmd = Some(String::from_utf8(bvalue.into())?);
+        }
+    }
+
+    let Some(name) = name else {
+        return Ok(None);
+    };
+
+    let Some(cmd) = cmd else {
+        return Ok(None);
+    };
+
+    if !isapp {
+        return Ok(None);
+    }
+    Ok(Some(DesktopEntry {
+        name,
+        cmd,
+        icon,
+        terminal,
+    }))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::Env;
+use iced::{
+    Element,
+    widget::{column, text_input},
+};
+
+mod discover;
+
+fn main() -> iced::Result {
+    env_logger::init_from_env(
+        Env::new()
+            .filter_or("TYLA_LOG", "info")
+            .write_style("TYLA_LOG_STYLE"),
+    );
+    discover::desktop_apps();
+    iced::run(App::update, App::view)
+}
+
+#[derive(Debug, Clone)]
+enum Msg {
+    SearchTextChanged(String),
+}
+
+#[derive(Default, Debug)]
+struct App {
+    search_text: String,
+}
+
+impl App {
+    fn update(&mut self, msg: Msg) {
+        match msg {
+            Msg::SearchTextChanged(txt) => self.search_text = txt,
+        }
+    }
+
+    fn view(&self) -> Element<'_, Msg> {
+        column![
+            text_input("Search term here...", &self.search_text).on_input(Msg::SearchTextChanged),
+            "Here goes the results"
+        ]
+        .into()
+    }
 }
